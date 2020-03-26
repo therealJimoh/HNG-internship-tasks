4fb396848aaeaec609dabd3c882337e9898bd93b
Abdullah Jimoh <jimoh.abdullah69@gmail.com>
2020-03-26 16:32:18 +0100

    JS task

---
+++ index.js
@@ -0,0 +1,10 @@
+var myName = 'therealJimoh';
+  console.log(myName);
+
+var myCourses = ['html', 'css', 'javascript'];
+  console.log(myCourses);
+
+
+for (let g = 1; g <=200; g++) {
+    if (g % 2 !== 0) console.log(g);
+}
